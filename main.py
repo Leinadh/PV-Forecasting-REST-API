8ac92acca8874623dcd6b2726e2abb6dbafeef94
Leinadh <daniel.ibanez@pucp.edu.pe>
2021-11-12 10:52:54 -0500

    fecha limite

--- main.py
+++ main.py
@@ -81,6 +81,8 @@ def listar_ubicaciones():
 def get_fechas_limite():
     fecha_min = datetime.date(2020, 2, 24)
     now = datetime.datetime.now()
+    now = datetime.datetime(2020, 10, 20)
+    
     fecha_max = now.date() if now.hour < 17 else now.date() + \
         datetime.timedelta(days=1)
     return {"min_date": fecha_min, "max_date": fecha_max}
